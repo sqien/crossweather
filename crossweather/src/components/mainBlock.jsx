0e0542e7a459201c0785294f9d041c538232a12f
sqien <twitchsqien@gmail.com>
2025-06-28 11:24:26 +0200

    glassmorphism

--- crossweather/src/components/mainBlock.jsx
+++ crossweather/src/components/mainBlock.jsx
@@ -44,14 +44,14 @@ function MainWindow() {
 
   return (
     <main className="flex justify-center items-start text-center w-screen h-screen">
-      <div className="location absolute flex justify-center items-center gap-2 mt-26">
+      <div className="location absolute flex justify-center items-center gap-2 mt-20 bg-white/10 p-2 rounded-xl border border-white/20">
         <MapPin />
-        <p className="text-3xl">Opole, Poland</p>
+        <p className="text-2xl">Opole, Poland</p>
       </div>
       <div className="window flex flex-col justify-between h-full">
         <div className="weather h-full flex justify-center items-center">
-          <div className="weather-container">
-            <div className="weather-container-item p-4 flex">
+          <div className="weather-container relative z-10 bg-white/10 backdrop-blur-lg border border-white/20 rounded-[2rem] p-10 w-[350px] text-white text-center shadow-xl">
+            <div className="weather-container-item p-4 flex justify-center">
               <div className="flex items-center text-3xl">
                 {weatherCode !== null && getWeatherIcon(weatherCode)}
               </div>
@@ -59,7 +59,7 @@ function MainWindow() {
                 {temperature}
               </h1>
             </div>
-            <div className="weather-container-item flex justify-center text-3xl gap-4">
+            <div className="weather-container-item flex justify-center text-3xl gap-4 p-2">
               <p className="time" id="time">
                 {time}
               </p>
@@ -68,14 +68,16 @@ function MainWindow() {
               </p>
             </div>
 
-            <div className="weather-container-footer grid grid-cols-2 gap-4 text-xl text-center">
+            <div className="weather-container-footer flex justify-center gap-4 p-2 text-md">
+                <div className="flex bg-white/10 p-3 rounded-xl border border-white/20 gap-2">
               <div className="weather-container-item flex justify-center items-center gap-2">
-                <Wind className="w-6 h-6 text-blue-400" />
+                <Wind className="w-6 h-6 text-blue-400 " />
                 <span>{wind}</span>
               </div>
               <div className="weather-container-item flex justify-center items-center gap-2">
                 <Droplet className="w-6 h-6 text-cyan-500" />
                 <span>{precipitation}</span>
+              </div>
               </div>
             </div>
           </div>
