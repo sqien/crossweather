3204333c6d0c7c8a7c7dd95b45740f1ac949ac2f
sqien <rymar2137@gmail.com>
2025-07-07 13:59:18 +0200

    update

--- src/components/ForecastCard.jsx
+++ src/components/ForecastCard.jsx
@@ -1,6 +1,5 @@
 import { getWeatherIcon } from "../utils/weatherIcons";
 import { Droplet } from "lucide-react";
-import { FooterCards } from "./FooterCards"
 
 export default function ForecastCard({ date, min, max, rain, code }) {
   const weekday = new Date(date).toLocaleDateString("en-GB", {
